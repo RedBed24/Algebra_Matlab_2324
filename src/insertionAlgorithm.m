87e83fb90beaa564139a49ae5bcfcd4caa22e93c
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2024-06-14 12:32:28 +0200

    ~ performant insertion algorithm

--- src/insertionAlgorithm.m
+++ src/insertionAlgorithm.m
@@ -1,26 +1,21 @@
 function [c] = insertionAlgorithm(d,v,n)
 c = [v v];
 
+rest = find(1:n ~= v);
+
 for i = 1:n - 1
 
-    % obtenemos v_i de forma aleatoria, asegurando que no esté ya contenido en c
-    v = randi([1 n]);
-    % mientras haya alguno en c que sea igual que v_i
-    while any(find(c == v))
-        % obtenemos otro aleatorio
-        v = randi([1 n]);
-    end
+    % obtenemos un índice aleatorio para el resto de vértices
+    index = randi([1 length(rest)]);
+    % tomamos ese vértice
+    v = rest(index);
+    % lo eliminamos del resto
+    rest = [rest(1:index-1), rest(index+1:length(rest))];
 
-    k = 1;
-    mini = k;
-    min_val = d(c(k), v) + d(v, c(k+1));
-    for k = 2:i
-        val = d(c(k), v) + d(v, c(k+1));
-        if val < min_val
-            mini = k;
-            min_val = val;
-        end
-    end
+    % hacemos todas las sumas con el vértice seleccionado
+    % w_k son los vértices de c
+    sums = d(c(1:length(c)-1), v) + d(v, c(2:length(c)));
+    k = find(sums == min(sums));
 
     c = [c(1:k), v, c(k+1:length(c))];
 
